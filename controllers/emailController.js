6dc0802a457b2536e23ef69f697055865b9782f4
wizardofcoding1 <152683729+wizardofcoding1@users.noreply.github.com>
2025-10-09 16:07:29 +0530

    Fix The Code With SMTP With Port 459

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -12,14 +12,14 @@ const sendEmail = async (req, res) => {
 
     // Create transporter with TLS fix for Render
     const transport = nodemailer.createTransport({
-      service: "gmail",
+      host:"smtp.gmail.com",
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS, // Use Gmail App Password
       },
-      tls: {
-        rejectUnauthorized: false,
-      },
+      
     });
 
     // Verify transporter
